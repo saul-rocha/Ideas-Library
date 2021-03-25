28ad7f30f47d5a42fb1a6d5ee61b19887decc830
saul-rocha <saul.rocha2001@gmail.com>
2021-03-25 10:53:04 -0300

    construção do cabeçalho

---
+++ scripts/scripts.js
@@ -0,0 +1,8 @@
+document
+    .querySelector("button.fat")
+    .addEventListener("click",function(){
+        document
+        .querySelector("#foother")
+        .classList
+        .toggle("hide")
+    })
